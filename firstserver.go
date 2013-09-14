a24af63e2acb6cac79bb60c38b38de37a3fec007
Reed Allman <rdallman10@gmail.com>
2013-09-13 21:42:01 -0500

    comment code

--- firstserver.go
+++ firstserver.go
@@ -6,13 +6,16 @@ import (
 )
 
 func simpleHandler(w http.ResponseWriter, r *http.Request) {
-  wordbey := r.URL.Path[1:]
+  wordbey := r.URL.Path[1:] // slice off the '/'
   fmt.Fprintf(w, "How much %s", wordbey)
   fmt.Fprintf(w, " would a %sbey chuck,", wordbey)
   fmt.Fprintf(w, "\nif a %sbey could chuck %s?", wordbey, wordbey)
+  // a handler can be as simple as a print...
 }
 
 func main() {
+  // takes a path and a func (http.ResponseWriter, r *http.Request)
   http.HandleFunc("/", simpleHandler)
+  // our server is one line!
   http.ListenAndServe(":8080", nil)
 }
